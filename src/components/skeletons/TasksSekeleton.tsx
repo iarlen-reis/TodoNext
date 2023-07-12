f312fff826a98e11dad416236a3294ca29e7b9ce
Iarlen <iarlensr@hotmail.com>
2023-07-11 23:04:30 -0300

    feat: creating Skeleton component for task listing.

---
+++ src/components/skeletons/TasksSekeleton.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const TasksSekeleton = () => {
+  return (
+    <div className="m-auto grid w-full animate-pulse grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+      <div className="m-auto mb-4 h-36 w-full max-w-[380px] rounded-md bg-gray-200 dark:bg-gray-400"></div>
+      <div className="m-auto mb-4 h-36 w-full max-w-[380px] rounded-md bg-gray-200 dark:bg-gray-400"></div>
+      <div className="m-auto mb-4 h-36 w-full max-w-[380px] rounded-md bg-gray-200 dark:bg-gray-400"></div>
+      <div className="m-auto mb-4 h-36 w-full max-w-[380px] rounded-md bg-gray-200 dark:bg-gray-400"></div>
+      <div className="m-auto mb-4 h-36 w-full max-w-[380px] rounded-md bg-gray-200 dark:bg-gray-400"></div>
+      <div className="m-auto mb-4 h-36 w-full max-w-[380px] rounded-md bg-gray-200 dark:bg-gray-400"></div>
+      <div className="m-auto mb-4 h-36 w-full max-w-[380px] rounded-md bg-gray-200 dark:bg-gray-400"></div>
+      <div className="m-auto mb-4 h-36 w-full max-w-[380px] rounded-md bg-gray-200 dark:bg-gray-400"></div>
+      <div className="m-auto mb-4 h-36 w-full max-w-[380px] rounded-md bg-gray-200 dark:bg-gray-400"></div>
+      <div className="m-auto mb-4 h-36 w-full max-w-[380px] rounded-md bg-gray-200 dark:bg-gray-400"></div>
+      <div className="m-auto mb-4 h-36 w-full max-w-[380px] rounded-md bg-gray-200 dark:bg-gray-400"></div>
+      <div className="m-auto mb-4 h-36 w-full max-w-[380px] rounded-md bg-gray-200 dark:bg-gray-400"></div>
+      <span className="sr-only">Loading...</span>
+    </div>
+  )
+}
+
+export default TasksSekeleton
