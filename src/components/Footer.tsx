a57e50b0c888374471474e53b95faa3970a5e1ab
Iarlen <iarlensr@hotmail.com>
2023-07-11 23:51:13 -0300

    refactor: changing Footer component structure.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,30 +1,23 @@
-import Link from 'next/link'
 import React from 'react'
 
 export const Footer = () => {
   return (
-    <footer className="flex w-full items-center justify-center bg-zinc-900">
-      <div className="flex w-full max-w-[1300px] items-center justify-between p-3 ">
-        <ul className="flex list-none flex-col gap-2">
-          <li className="font-body text-sm text-zinc-50 hover:text-zinc-200">
-            <Link href="/">Página inicial</Link>
-          </li>
-          <li className="font-body text-sm text-zinc-50 hover:text-zinc-200">
-            <Link href="/">Sobre</Link>
-          </li>
-        </ul>
-        <ul className="flex list-none items-center">
-          <li>
-            <a
-              href="https://github.com/iarlen-reis"
-              rel="noreferrer"
-              target="_blank"
-              className="font-body italic text-zinc-50 hover:text-zinc-200"
-            >
-              Feito por Iarlen Reis
-            </a>
-          </li>
-        </ul>
+    <footer className="flex h-20 w-full items-center justify-center bg-zinc-900">
+      <div className="flex w-full max-w-[1300px] flex-col items-center justify-between gap-2 p-3 sm:flex-row ">
+        <p className="font-body text-xs leading-relaxed text-zinc-50 sm:text-lg">
+          &copy; TodoNext 2023
+        </p>
+        <p className="font-body text-xs leading-relaxed text-zinc-50 sm:text-lg">
+          Desenvolvido por{' '}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/iarlen-reis"
+            className="text-zinc-50 no-underline hover:underline"
+          >
+            Iarlen Reis
+          </a>
+        </p>
       </div>
     </footer>
   )
