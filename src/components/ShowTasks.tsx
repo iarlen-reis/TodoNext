c97a2d1c0df623335a6ed5e3eff2b625239fc5b0
Iarlen <iarlensr@hotmail.com>
2023-07-11 23:17:44 -0300

    refactor: adding TaskSkeleton in ShowTasks component.

--- src/components/ShowTasks.tsx
+++ src/components/ShowTasks.tsx
@@ -1,23 +1,27 @@
 'use client'
 import { useTasks } from '@/hooks/useTasks'
 import { TodoCard } from './TodoCard'
+import TasksSekeleton from './skeletons/TasksSekeleton'
 
 export const ShowTasks = () => {
-  const { tasks } = useTasks()
+  const { tasks, tasksLoading } = useTasks()
 
   return (
-    <div className="grid w-full grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
-      {tasks &&
-        tasks.map((task) => (
-          <TodoCard
-            id={task.id}
-            title={task.title}
-            dateConclusion={task.dateConclusion}
-            status={task.status}
-            key={task.id}
-            color={task.color}
-          />
-        ))}
-    </div>
+    <>
+      <div className="grid w-full grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+        {tasks &&
+          tasks.map((task) => (
+            <TodoCard
+              id={task.id}
+              title={task.title}
+              dateConclusion={task.dateConclusion}
+              status={task.status}
+              key={task.id}
+              color={task.color}
+            />
+          ))}
+      </div>
+      {tasksLoading && <TasksSekeleton />}
+    </>
   )
 }
