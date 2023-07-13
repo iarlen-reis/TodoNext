4eb9380df4798239ec0ee34579be298c27d0b020
Iarlen <iarlensr@hotmail.com>
2023-07-13 19:25:30 -0300

    feat: creating skeleton component of MenuTools.

--- src/components/MenuTools.tsx
+++ src/components/MenuTools.tsx
@@ -1,18 +1,35 @@
+'use client'
 import React from 'react'
 import { Filter, Plus } from 'lucide-react'
+import { useTasks } from '@/hooks/useTasks'
 import Link from 'next/link'
+import { MenuToolsSkeleton } from './skeletons/MenuToolsSkeleton'
+
 export const MenuTools = () => {
+  const { tasksLoading } = useTasks()
   return (
     <div className="flex items-center justify-end gap-4">
-      <button className="group flex h-11 w-11 items-center justify-center rounded-full py-1 transition-colors hover:bg-zinc-500">
-        <Filter size={20} className="text-zinc-800 group-hover:text-zinc-300" />
-      </button>
-      <Link
-        href="/create"
-        className="group flex h-11 w-11 items-center justify-center rounded-full py-1 transition-colors hover:bg-zinc-500"
-      >
-        <Plus size={20} className="text-zinc-800  group-hover:text-zinc-300" />
-      </Link>
+      {!tasksLoading ? (
+        <>
+          <button className="group flex h-11 w-11 items-center justify-center rounded-full py-1 transition-colors hover:bg-zinc-900">
+            <Filter
+              size={20}
+              className="text-zinc-900 group-hover:text-zinc-100"
+            />
+          </button>
+          <Link
+            href="/create"
+            className="group flex h-11 w-11 items-center justify-center rounded-full py-1 transition-colors hover:bg-zinc-900"
+          >
+            <Plus
+              size={20}
+              className="text-zinc-900  group-hover:text-zinc-100"
+            />
+          </Link>
+        </>
+      ) : (
+        <MenuToolsSkeleton />
+      )}
     </div>
   )
 }
---
+++ src/components/skeletons/MenuToolsSkeleton.tsx
@@ -0,0 +1,8 @@
+export const MenuToolsSkeleton = () => {
+  return (
+    <>
+      <div className="flex h-11 w-11 animate-pulse rounded-full bg-zinc-400"></div>
+      <div className="flex h-11 w-11 animate-pulse rounded-full bg-zinc-400"></div>
+    </>
+  )
+}
