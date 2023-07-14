09476666d380b492e60d2033ddd341ef874fbd21
Iarlen <iarlensr@hotmail.com>
2023-07-14 02:59:25 -0300

    feat: adding functionality to display the filter in the filterContext.

--- src/contexts/filterContext.tsx
+++ src/contexts/filterContext.tsx
@@ -2,12 +2,16 @@ import { ReactNode, createContext, useContext, useState } from 'react'
 
 interface IFilterContextProps {
   filter: string
+  optionsFilter: boolean
   handleFilter: (value: string) => void
+  handleOptionsFilter: () => void
 }
 
 export const FilterContext = createContext<IFilterContextProps>({
   filter: '',
+  optionsFilter: false,
   handleFilter: (value: string) => {},
+  handleOptionsFilter: () => {},
 })
 
 interface IChildren {
@@ -16,13 +20,20 @@ interface IChildren {
 
 export const FilterProvider = ({ children }: IChildren) => {
   const [filter, setFilter] = useState<string>('')
+  const [optionsFilter, setOptionsFilter] = useState<boolean>(false)
 
   const handleFilter = (value: string) => {
     setFilter(value)
   }
 
+  const handleOptionsFilter = () => {
+    setOptionsFilter(!optionsFilter)
+  }
+
   return (
-    <FilterContext.Provider value={{ filter, handleFilter }}>
+    <FilterContext.Provider
+      value={{ filter, optionsFilter, handleFilter, handleOptionsFilter }}
+    >
       {children}
     </FilterContext.Provider>
   )
