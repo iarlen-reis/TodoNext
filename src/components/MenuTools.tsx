50c70955b81097eb5b9907f922a8c0fe566b56a2
Iarlen <iarlensr@hotmail.com>
2023-07-14 02:50:59 -0300

    feat: adding FilterOptions component in MenuTools component.

--- src/components/MenuTools.tsx
+++ src/components/MenuTools.tsx
@@ -1,31 +1,41 @@
 'use client'
 import React from 'react'
-import { Filter, Plus } from 'lucide-react'
-import { useTasks } from '@/hooks/useTasks'
 import Link from 'next/link'
+import { FilterOptions } from '@/components/FilterOptions'
+import { useFilterContext } from '@/contexts/filterContext'
 import { MenuToolsSkeleton } from './skeletons/MenuToolsSkeleton'
+import { useTasks } from '@/hooks/useTasks'
+import { Filter, Plus } from 'lucide-react'
 
 export const MenuTools = () => {
   const { tasksLoading } = useTasks()
+  const { handleOptionsFilter, optionsFilter } = useFilterContext()
+
   return (
-    <div className="flex items-center justify-end gap-4">
+    <div className="flex flex-col gap-3">
       {!tasksLoading ? (
         <>
-          <button className="group flex h-11 w-11 items-center justify-center rounded-full py-1 transition-colors hover:bg-zinc-900">
-            <Filter
-              size={20}
-              className="text-zinc-900 group-hover:text-zinc-100"
-            />
-          </button>
-          <Link
-            href="/create"
-            className="group flex h-11 w-11 items-center justify-center rounded-full py-1 transition-colors hover:bg-zinc-900"
-          >
-            <Plus
-              size={20}
-              className="text-zinc-900  group-hover:text-zinc-100"
-            />
-          </Link>
+          <div className="flex items-center justify-end gap-3">
+            <button
+              onClick={handleOptionsFilter}
+              className="group flex h-11 w-11 items-center justify-center rounded-full py-1 transition-colors hover:animate-pulse  hover:bg-zinc-900"
+            >
+              <Filter
+                size={20}
+                className="text-zinc-900 group-hover:text-zinc-100"
+              />
+            </button>
+            <Link
+              href="/create"
+              className="group flex h-11 w-11 items-center justify-center rounded-full py-1 transition-colors hover:animate-pulse hover:bg-zinc-900"
+            >
+              <Plus
+                size={20}
+                className="text-zinc-900 group-hover:text-zinc-100"
+              />
+            </Link>
+          </div>
+          {optionsFilter && <FilterOptions />}
         </>
       ) : (
         <MenuToolsSkeleton />
