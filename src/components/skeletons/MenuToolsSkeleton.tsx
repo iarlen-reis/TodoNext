94eff928ca38715124f3a7b619446ce121938ce2
Iarlen <iarlensr@hotmail.com>
2023-07-14 03:01:31 -0300

    refactor: correcting MenuTools component skeleton.

--- src/components/skeletons/MenuToolsSkeleton.tsx
+++ src/components/skeletons/MenuToolsSkeleton.tsx
@@ -1,8 +1,8 @@
 export const MenuToolsSkeleton = () => {
   return (
-    <>
+    <div className="flex items-center justify-end gap-3">
       <div className="flex h-11 w-11 animate-pulse rounded-full bg-zinc-400"></div>
       <div className="flex h-11 w-11 animate-pulse rounded-full bg-zinc-400"></div>
-    </>
+    </div>
   )
 }
