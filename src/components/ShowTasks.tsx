bf1d960604014c1291d34f911dfc4e3761c51cdf
Iarlen <iarlensr@hotmail.com>
2023-07-14 02:52:06 -0300

    feat: configuring filter in the ShowTasks component.

--- src/components/ShowTasks.tsx
+++ src/components/ShowTasks.tsx
@@ -1,16 +1,20 @@
 'use client'
-import { useTasks } from '@/hooks/useTasks'
 import { TodoCard } from './TodoCard'
+import { useTasks } from '@/hooks/useTasks'
+import { useFilterContext } from '@/contexts/filterContext'
 import TasksSekeleton from './skeletons/TasksSekeleton'
 
 export const ShowTasks = () => {
   const { tasks, tasksLoading } = useTasks()
+  const { filter } = useFilterContext()
+
+  const filtered = tasks?.filter((task) => task.status.includes(filter))
 
   return (
     <>
       <div className="grid w-full grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
-        {tasks &&
-          tasks.map((task) => (
+        {filtered &&
+          filtered.map((task) => (
             <TodoCard
               id={task.id}
               title={task.title}
