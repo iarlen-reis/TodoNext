71269fc2a30a6c44a9cfefa08ac47aa99b2c5890
Iarlen <iarlensr@hotmail.com>
2023-07-18 02:31:01 -0300

    refactor: adding preview for non-logged-in users on homepage.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
+import { HomeImage } from '@/components/HomeImage'
+import { HomeTitle } from '@/components/HomeTitle'
 import { MenuTools } from '@/components/MenuTools'
 import { ShowTasks } from '@/components/ShowTasks'
+import { useGetUser } from '@/hooks/useGetUser'
 
 export default async function Home() {
+  const { session } = await useGetUser()
   return (
-    <div className="mt-3 flex w-full flex-col justify-center gap-6">
-      <MenuTools />
-      <ShowTasks />
-    </div>
+    <main className="flex flex-col pb-10">
+      {session ? (
+        <div className="mt-3 flex w-full flex-col justify-center gap-6">
+          <MenuTools />
+          <ShowTasks />
+        </div>
+      ) : (
+        <div className="flex flex-col gap-4">
+          <HomeTitle />
+          <HomeImage />
+        </div>
+      )}
+    </main>
   )
 }
