129e10ac345735bb3e1660b442983b27d52d3075
Iarlen <iarlensr@hotmail.com>
2023-07-18 18:20:32 -0300

    fix: fixing lint error in prisma.ts and nex-auth-d.ts file

--- src/Types/next-auth.d.ts
+++ src/Types/next-auth.d.ts
@@ -1,12 +1,9 @@
-import NextAuth, { DefaultSession } from 'next-auth'
+/* eslint-disable no-unused-vars */
+import { DefaultSession } from 'next-auth'
 
 declare module 'next-auth' {
-  /**
-   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
-   */
   interface Session {
     user: {
-      /** The user's postal address. */
       id?: string
     } & DefaultSession['user']
   }
