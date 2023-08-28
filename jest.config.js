ff5176bb30260caa316632662354fc73ecc6dd62
Iarlen <iarlensr@hotmail.com>
2023-08-28 01:39:19 -0300

    feat: installing and configuring jest and testing library.

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+}
+
+module.exports = createJestConfig(customJestConfig)
